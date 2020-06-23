2401ff9a030d4c1ba6525d99f69b3a2df14323d0
BenWix <benwichser@gmail.com>
2020-06-23 17:12:36 -0400

    Done.

--- index.js
+++ index.js
@@ -1,1 +1,32 @@
 // Your code here
+let dodger = document.getElementById("dodger");
+let gameWindow = document.getElementById("game");
+let speed = 5;
+
+function moveDodgerLeft() {
+   let leftNumbers = dodger.style.left.replace("px", "");
+   let left = parseInt(leftNumbers, 10); 
+
+   if (left > 0) {
+       dodger.style.left = `${left - speed }px`
+   }
+}
+
+function moveDodgerRight() {
+    let leftNumbers = dodger.style.left.replace("px", "");
+    let left = parseInt(leftNumbers, 10); 
+ 
+    if (left <  360) {
+        dodger.style.left = `${left + speed}px`
+    }
+}
+
+
+document.addEventListener("keydown", function(e) {
+    if (e.key === "ArrowLeft") {
+        moveDodgerLeft();
+    } else if (e.key === "ArrowRight") {
+        moveDodgerRight();
+    }
+
+})
